60430b8e16cf027f71d37126479da96e4e09f17b
renato93mg <102925391+renato93mg@users.noreply.github.com>
2023-01-22 04:41:06 -0500

    Add files via upload

---
+++ app01/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ListadoComponent } from './components/listado/listado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app01/src/app/components/listado/listado.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Alumno } from './alumno';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public titulo:string="Tabla de alumnos"
+
+  public alumnos:Alumno[]=[
+    {id:1,nombre:'Juan',apellido:'Perez',edad:12},
+    {id:2,nombre:'Paulino',apellido:'Ramirez',edad:19},
+    {id:3,nombre:'Paco',apellido:'Gomez',edad:11},
+    {id:4,nombre:'Pedro',apellido:'Martinez',edad:20},
+  ]
+
+}
+
+
